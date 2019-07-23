7d6c55f31194a6355918b092dc4a3fba0fa39b65
Ykie <kieyg@gmail.com>
2019-07-24 00:30:14 +0900

    axios > vuex default setting 

--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,10 +1,22 @@
 <template>
-    <div>Jobs</div>
+    <div>
+        <div v-for="item in jobs" :key="item.id">{{ item.title }}</div>
+    </div>
 </template>
 
 <script>
+import { fetchJobsList } from '../api/index.js';
 export default {
-
+    data() {
+        return{
+            jobs: []
+        }
+    },
+    created() {
+        fetchJobsList()
+            .then(response => this.jobs = response.data)
+            .catch(err => console.log(err));
+    },
 }
 </script>
 
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,10 +1,20 @@
 <template>
-    <div>News</div>
+    <div>
+        <div v-for="user in users" :key="user.id">{{ user.title }}</div>
+    </div>
 </template>
 
 <script>
+import { fetchNewsList } from '../api/index.js';
 export default {
-
+    data() {
+        return {
+            users: []
+        }
+    },
+    created() {
+        this.$store.dispatch('FETCH_NEWS');
+    },
 }
 </script>
 
